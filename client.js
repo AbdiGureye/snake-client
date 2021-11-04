923430fe4bf34db61487369137cbf7099414c1c0
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-04 01:02:05 +0000

    intials

--- client.js
+++ client.js
@@ -4,14 +4,17 @@ const connect = function () {
     host: "165.227.47.243",
     port: 50541
   });
-  conn.on('data', data => {
-//     // code that does something when the connection is first established
-    console.log(data)
-  });
-//   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on('connect', data => {
+//     // code that does something when the connection is first established
+    console.log("Successfully connected to game server")
+  });
+ conn.write("Name: AAG")
+//   // interpret incoming data as text
+  
 
   return conn;
 };
 
 module.exports = connect;
+
