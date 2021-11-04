07bc00467f479253b22ca066168893c9aa1dfc84
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-04 01:44:09 +0000

    terminate edit

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 // const net = require("net");
 const connect = require(`./client`);
-
+const handleUserInput = function (value) {
+   
+  if (value === '\u0003') {
+    console.log("Terminated")
+    process.exit();
+  }
+  
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}; 
+setupInput()
 
 // // establishes a connection with the game server
 // const connect = function () {
