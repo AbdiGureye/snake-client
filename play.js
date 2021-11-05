aa0786de8650bfd6c33a1547c937322888193a17
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-05 00:17:16 +0000

    play.js

--- play.js
+++ play.js
@@ -1,14 +1,15 @@
 // const net = require("net");
 const setupInput = require(`./input`);
 const connect = require(`./client`);
-const handleUserInput = function (value) {
+const net = require("net");
+// const handleUserInput = function (value) {
    
-  if (value === '\u0003') {
-    console.log("Terminated")
-    process.exit();
-  }
+//   // if (value === '\u0003') {
+//   //   console.log("Terminated")
+//   //   process.exit();
+//   // }
   
-}
+// }
 // const setupInput = function () {
 //   const stdin = process.stdin;
 //   stdin.setRawMode(true);
@@ -17,24 +18,24 @@ const handleUserInput = function (value) {
 //   stdin.on("data", handleUserInput);
 //   return stdin;
 // }; 
-setupInput()
 
 // // establishes a connection with the game server
 // const connect = function () {
-//   const conn = net.createConnection({
-//     host: "165.227.47.243",
-//     port: 50541
-//   });
-//   conn.on('data', data => {
-// //     // code that does something when the connection is first established
-//     console.log(data)
-//   });
-// //   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   return conn;
-// };
-
-
-console.log("Connecting ...");
-connect();
+  //   const conn = net.createConnection({
+    //     host: "165.227.47.243",
+    //     port: 50541
+    //   });
+    //   conn.on('data', data => {
+      // //     // code that does something when the connection is first established
+      //     console.log(data)
+      //   });
+      // //   // interpret incoming data as text
+      //   conn.setEncoding("utf8");
+      
+      //   return conn;
+      // };
+      
+      
+      console.log("Connecting ...");
+      let connection = connect();
+      setupInput(connection);
